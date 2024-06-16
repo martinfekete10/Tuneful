60435c4e8cdbfd81d70ef41c4ddb1bf168ac54f9
Martin Fekete <martinfekete10@gmail.com>
2024-06-16 14:47:48 +0200

    Pipeline fix

--- Configuration/generate_latest_changes.py
+++ Configuration/generate_latest_changes.py
@@ -8,13 +8,12 @@ def get_latest_release_notes():
         first_header = lines[0].strip()
         title = first_header.split(' - ')[1]
         new_version = first_header.split(' - ')[0].replace('# ', '')
-        lines = lines[1:]
+        lines = lines[2:]
         latest_changes = ""
         for line in lines:
-            if line.strip().startswith('*'):
+            if line.strip().startswith('-'):
                 latest_changes += line.strip() + "\n"
             elif line.strip().startswith('#'):
-                old_version = line.strip().split(' - ')[0].replace('# ', '')
                 break
         return title, new_version, latest_changes
 
