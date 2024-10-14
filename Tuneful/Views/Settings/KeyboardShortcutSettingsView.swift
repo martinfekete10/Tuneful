d20ad1e6a63e9644f51d42c98e4d691c7aa882b5
Martin Fekete <martinfekete10@gmail.com>
2024-10-14 18:16:02 +0200

    Keyboard settings updated

--- Tuneful/Views/Settings/KeyboardShortcutSettingsView.swift
+++ Tuneful/Views/Settings/KeyboardShortcutSettingsView.swift
@@ -8,23 +8,24 @@
 import SwiftUI
 import Settings
 import KeyboardShortcuts
+import Luminare
 
 struct KeyboardShortcutsSettingsView: View {
     var body: some View {
-        Settings.Container(contentWidth: 350) {
+        Settings.Container(contentWidth: 400) {
             Settings.Section(title: "") {
-                VStack(alignment: .center, content: {
+                LuminareSection {
                     Form {
-                        KeyboardShortcuts.Recorder("Play/pause:", name: .playPause)
-                        KeyboardShortcuts.Recorder("Next track:", name: .nextTrack)
-                        KeyboardShortcuts.Recorder("Previous track:", name: .previousTrack)
-                        KeyboardShortcuts.Recorder("Show/hide mini player:", name: .showMiniPlayer)
-                        KeyboardShortcuts.Recorder("Switch music player:", name: .changeMusicPlayer)
-                        KeyboardShortcuts.Recorder("Show/hide menu bar player:", name: .toggleMenuBarItemVisibility)
-                        KeyboardShortcuts.Recorder("Show/hide popover:", name: .togglePopover)
+                        KeyboardShortcuts.Recorder("Play/pause", name: .playPause)
+                        KeyboardShortcuts.Recorder("Next track", name: .nextTrack)
+                        KeyboardShortcuts.Recorder("Previous track", name: .previousTrack)
+                        KeyboardShortcuts.Recorder("Show/hide mini player", name: .showMiniPlayer)
+                        KeyboardShortcuts.Recorder("Switch music player", name: .changeMusicPlayer)
+                        KeyboardShortcuts.Recorder("Show/hide menu bar player", name: .toggleMenuBarItemVisibility)
+                        KeyboardShortcuts.Recorder("Show/hide popover", name: .togglePopover)
                     }
                     .frame(maxWidth: .infinity, alignment: .center)
-                })
+                }
             }
         }
     }
